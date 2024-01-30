f7bbe043ac8f2f088784a50d4fedda4757a98abc
Marcel Spannagel <spannagel.marcel@gmail.com>
2024-01-30 14:46:00 +0100

    add HttpClient to cart service

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Product } from './products';
 import { Injectable } from '@angular/core';
 
@@ -7,19 +8,25 @@ import { Injectable } from '@angular/core';
 export class CartService {
   items: Product[] = [];
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
   
   addToCart(product: Product){
     this.items.push(product);
   }
 
+  clearItems(){
+    this.items = [];
+    return this.items;
+  }
+  
   getItems(){
     return this.items;
   }
 
-  clearItems(){
-    this.items = [];
-    return this.items;
+  getSippingPrices(){
+    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
   }
   
 }
